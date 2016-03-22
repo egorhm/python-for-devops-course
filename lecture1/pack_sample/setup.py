6ec15cfd9d348cb28331d7f22889233b098a1a2a
Egor Hamaliy <egor.hamaliy@gmail.com>
2016-03-22 19:48:54 +0300

    Directory structure changed

--- lecture1/pack_sample/setup.py
+++ lecture1/pack_sample/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="packaging-example",
-    packages=['useful-package', 'tests'],
+    name="pack_sample",
+    packages=['pack_sample', 'pack_sample.tests'],
     version="0.1",
     author="Egor Hamaliy",
     author_email="egor.hamaliy@gmail.com",
